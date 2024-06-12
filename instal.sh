ba7b65f07f8adb45d665556b427d98c5bde4ecb4
David Ramia <84158523+ramiadavid@users.noreply.github.com>
2024-06-12 07:33:22 +0200

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 hostnamectl set-hostname "master-node"
+apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt install -y kubelet kubeadm kubectl docker.io
+mkdir /etc/containerd
+sh -c "containerd config default > /etc/containerd/config.toml"
+sed -i 's/ SystemdCgroup = false/ SystemdCgroup = true/' /etc/containerd/config.toml
+systemctl restart containerd.service
+systemctl restart kubelet.service
+systemctl enable kubelet.service
+kubeadm config images pull
+kubeadm init --config config.yaml
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
