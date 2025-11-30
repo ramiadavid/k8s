78142b647ac6aa878152ff7737ace795adf25dbd
David Ramia <84158523+ramiadavid@users.noreply.github.com>
2025-11-30 21:42:28 +0100

    Update instal.sh

--- instal.sh
+++ instal.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 hostnamectl set-hostname "master-node"
 apt-get install -y apt-transport-https ca-certificates curl
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.34/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt install -y kubelet kubeadm kubectl docker.io
 mkdir /etc/containerd
